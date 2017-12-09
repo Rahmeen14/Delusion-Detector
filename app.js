eebfed42ddc42cd8cb81f769908d0ff899d27dc3
Rahmeen14 <rahmeenwill99@gmail.com>
2017-12-09 15:30:48 +0530

    Basic functionality set!

---
+++ app.js
@@ -0,0 +1,93 @@
+var express = require("express"),
+	bodyParser= require("body-parser"),
+    app       = express(),
+    fileUpload = require('express-fileupload');
+var PythonShell = require('python-shell'),
+sys = require('sys');
+var spawn = require("child_process").spawn;
+
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.set("view engine", "ejs");
+app.use(fileUpload());
+app.use(express.static(__dirname));
+
+app.get("/", function(req, res){
+
+	res.render("upload");
+});
+app.post('/upload', function(req, res) {
+  if (!req.files)
+    return res.status(400).send('No files were uploaded.');
+ 
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  console.log(req.body);
+  let sampleFile = req.files.sampleFile;
+  var filename = req.files.sampleFile.name;
+  var level = parseFloat(req.body.level);
+  var claim = parseInt(req.body.claim);
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv(__dirname+'/data/'+filename+'.csv', function(err) {
+    if (err)
+      return res.status(500).send(err);
+ 	
+ /*	 var process = spawn('python',["./cart.py", filename]);
+ 	 process.stdout.on('data', function (data){
+// Do something with the data returned from python script
+	console.log(data);
+
+
+});*/
+  var options = {
+  mode: 'text',
+  pythonPath: "C:/Users/hp/Anaconda3-2/python.exe",
+  pythonOptions: ['-u'],
+  scriptPath: './',
+  args: [filename, level]
+};
+
+var shell = new PythonShell('cart.py', options);
+shell.on('message', function (message) {
+	var len = message.length;
+	message = message.slice(2, len-1);
+	console.log(parseInt(message));
+  res.render("answer", {message: message, claim: claim});
+
+});
+
+/*PythonShell.run('cart.py', options, function (err, results) {
+  if (err) throw err;
+// results is an array consisting of messages collected during execution
+//  PythonShell.stdout.on('data', function (data){
+// Do something with the data returned from python script
+//console.log(data);
+//var pyshell = ;
+console.log(results);
+
+
+ // (new PythonShell('cart.py')).stdout.on('data', function (message) {
+    // received a message sent from the Python script (a simple "print" statement)
+    //sys.print(message.toString());
+//});
+
+// end the input stream and allow the process to exit
+
+
+});
+
+*/
+//res.send('File uploaded!');
+  });
+});
+
+app.listen(3000, "localhost", function(err){
+	console.log("Server on duty, Mallady!");
+});
+
+
+
+
+
+
+
+
---
+++ cart.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  8 20:47:01 2017
+
+@author: hp
+"""
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv(sys.argv[1]+".csv")
+
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+#from sklearn.tree import DecisionTreeRegressor
+#regressor2 = DecisionTreeRegressor(random_state = 0)
+#regressor2.fit(X, y)
+
+
+
+val = (float(sys.argv[2]))
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 300,random_state = 0)
+regressor.fit(X, y)
+y_pred = regressor.predict(val);
+
+print(y_pred)
+X_grid = np.arange(min(X), max(X), 0.01) 
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (DRT)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+
+sys.stdout.flush()
